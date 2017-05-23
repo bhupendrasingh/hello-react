508d25cb2402945a1e8a143932e4f86a55c7c422
bhupendrasingh <bhupendrasingh@ccs.neu.edu>
2017-05-22 23:34:46 -0400

    test clock component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Clock from './Clock';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <div>
+  <App />
+  <Clock/>,
+  </div>,
   document.getElementById('root')
 );
